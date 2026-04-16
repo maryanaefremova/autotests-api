44f3894b125961041d96621608f9b5c1f8aa7c1a
maryanaefremova <maryana.efremova@gmail.com>
2026-04-16 16:40:56 +0300

    Практикуемся в работе с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -8,13 +8,11 @@ login_payload = {
 login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
 login_response_data = login_response.json()
 
-print("Login response:", login_response_data)
-print("Status Code:", login_response.status_code)
+get_user_headers = {
+    "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+}
+get_user_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=get_user_headers)
+get_user_response_data = get_user_response.json()
 
-user_header = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
-
-user_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=user_header)
-user_response_data = user_response.json()
-
-print("User response:", user_response_data)
-print("Status Code:", user_response.status_code)
+print("Get user response:", get_user_response_data)
+print("Get user status code:", get_user_response.status_code)
