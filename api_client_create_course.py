740dc12a694c8e0b38f9754e7130283a70ca3776
maryanaefremova <maryana.efremova@gmail.com>
2026-04-20 18:59:42 +0300

    Применяем Pydantic в API клиентах. Практикуемся в применении Pydantic в API клиентах

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -1,10 +1,10 @@
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
+from clients.courses.courses_client import get_courses_client
 from clients.files.files_client import get_files_client
-# Вместо CreateFileRequestDict импортируем CreateFileRequestSchema
 from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
+from clients.courses.courses_schema import CreateCourseRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
@@ -25,7 +25,6 @@ authentication_user = AuthenticationUserSchema(
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
-# Вместо CreateFileRequestDict используем CreateFileRequestSchema
 create_file_request = CreateFileRequestSchema(
     filename="image.png",
     directory="courses",
@@ -34,14 +33,14 @@ create_file_request = CreateFileRequestSchema(
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
-create_course_request = CreateCourseRequestDict(
+create_course_request = CreateCourseRequestSchema(
     title="Python",
-    maxScore=100,
-    minScore=10,
+    max_score=100,
+    min_score=10,
     description="Python API course",
-    estimatedTime="2 weeks",
-    previewFileId=create_file_response.file.id,  # Используем атрибуты место ключей
-    createdByUserId=create_user_response.user.id  # Используем атрибуты место ключей
+    estimated_time="2 weeks",
+    preview_file_id=create_file_response.file.id,
+    created_by_user_id=create_user_response.user.id
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
