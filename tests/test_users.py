3586a88809a6f652e7ed418151919f7c1badc19e
maryanaefremova <maryana.efremova@gmail.com>
2026-04-23 21:43:44 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,13 +9,14 @@ from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
-    # Удалили инициализацию API клиента из теста
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(domain: str, public_users_client: PublicUsersClient):
+    request = CreateUserRequestSchema(email=fake.email(domain=domain))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
