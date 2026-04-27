298309d378e1f7753bef297d765bbd44c49458da
maryanaefremova <maryana.efremova@gmail.com>
2026-04-27 18:42:29 +0300

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -24,11 +24,14 @@ from tools.assertions.schema import validate_json_schema
 @allure.tag(AllureTag.EXERCISES, AllureTag.REGRESSION)
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.EXERCISES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.EXERCISES)
 class TestExercises:
     @allure.tag(AllureTag.CREATE_ENTITY)
     @allure.story(AllureStory.CREATE_ENTITY)  
     @allure.title("Create exercise")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
         request = CreateExerciseRequestSchema(course_id=function_course.response.course.id)
         response = exercises_client.create_exercise_api(request)
@@ -43,6 +46,7 @@ class TestExercises:
     @allure.story(AllureStory.GET_ENTITY)
     @allure.title("Get exercise")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExcersiceFixture):
         response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
         response_data = GetExerciseResponseSchema.model_validate_json(response.text)
@@ -56,6 +60,7 @@ class TestExercises:
     @allure.story(AllureStory.UPDATE_ENTITY)
     @allure.title("Update exercise")
     @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
     def test_update_exercise(self, exercises_client: ExercisesClient, function_exercise: ExcersiceFixture):
         request = UpdateExerciseRequestSchema()
         response = exercises_client.update_exercise_api(function_exercise.response.exercise.id, request)
@@ -70,6 +75,7 @@ class TestExercises:
     @allure.story(AllureStory.DELETE_ENTITY)
     @allure.title("Delete exercise")
     @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.DELETE_ENTITY)
     def test_delete_exercise(self, exercises_client: ExercisesClient, function_exercise: ExcersiceFixture):
         delete_response = exercises_client.delete_exercise_api(function_exercise.response.exercise.id)
 
@@ -87,6 +93,7 @@ class TestExercises:
     @allure.story(AllureStory.GET_ENTITIES)
     @allure.title("Get exercises")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITIES)
     def test_get_exercises(
             self,
             exercises_client: ExercisesClient,
