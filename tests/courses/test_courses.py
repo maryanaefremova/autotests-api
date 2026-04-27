3a365c6be2aa953a5e5944cbb41419989bdcf230
maryanaefremova <maryana.efremova@gmail.com>
2026-04-27 18:24:41 +0300

    Интеграция Allure в автотесты

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 
+import allure
 import pytest
+from allure_commons.types import Severity
 
 from clients.courses.courses_client import CoursesClient
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, GetCoursesQuerySchema, \
@@ -8,13 +10,24 @@ from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCour
 from fixtures.courses import CourseFixture
 from fixtures.files import FileFixture
 from fixtures.users import UserFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.courses import assert_create_course_response, assert_update_course_response, assert_get_courses_response
 from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.COURSES, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
 class TestCourses:
+    @allure.tag(AllureTag.GET_ENTITIES)
+    @allure.story(AllureStory.GET_ENTITIES)
+    @allure.title("Get courses")
+    @allure.severity(Severity.BLOCKER)
     def test_get_courses(
             self,
             courses_client: CoursesClient,
@@ -35,7 +48,11 @@ class TestCourses:
         
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
-        
+
+    @allure.tag(AllureTag.UPDATE_ENTITY)
+    @allure.story(AllureStory.UPDATE_ENTITY)
+    @allure.title("Update course")
+    @allure.severity(Severity.CRITICAL)    
     def test_update_course(self, courses_client: CoursesClient, function_course: CourseFixture):
         # Формируем данные для обновления
         request = UpdateCourseRequestSchema()
@@ -52,6 +69,10 @@ class TestCourses:
         # Валидируем JSON-схему ответа
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.title("Create course")
+    @allure.severity(Severity.BLOCKER)
     def test_create_course(self, courses_client: CoursesClient, function_user: UserFixture, function_file: FileFixture):
         request = CreateCourseRequestSchema(preview_file_id=function_file.response.file.id, created_by_user_id=function_user.response.user.id)
         response = courses_client.create_course_api(request)
--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -1,11 +1,17 @@
 from http import HTTPStatus
 
+import allure
 import pytest
+from allure_commons.types import Severity
 
 from clients.errors_schema import InternalErrorResponseSchema, ValidationErrorResponseSchema
 from clients.files.files_client import FilesClient
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema, GetFileResponseSchema
 from fixtures.files import FileFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.files import assert_create_file_response, assert_create_file_with_empty_directory_response, assert_create_file_with_empty_filename_response, assert_file_not_found_response, assert_get_file_response, assert_get_file_with_incorrect_file_id_response
 from tools.assertions.schema import validate_json_schema
@@ -13,7 +19,14 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.files
 @pytest.mark.regression
+@allure.tag(AllureTag.FILES, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.FILES)
 class TestFiles:
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.title("Create file")
+    @allure.severity(Severity.BLOCKER)
     def test_create_file(self, files_client: FilesClient):
         request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
         response = files_client.create_file_api(request)
@@ -24,6 +37,10 @@ class TestFiles:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.tag(AllureTag.GET_ENTITY)
+    @allure.story(AllureStory.GET_ENTITY)
+    @allure.title("Get file")
+    @allure.severity(Severity.BLOCKER)
     def test_get_file(self, files_client: FilesClient, function_file: FileFixture):
         response = files_client.get_file_api(function_file.response.file.id)
         response_data = GetFileResponseSchema.model_validate_json(response.text)
@@ -33,38 +50,10 @@ class TestFiles:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-    def test_create_file_with_empty_filename(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(
-            filename="",
-            upload_file="./testdata/files/image.png"
-        )
-        response = files_client.create_file_api(request)
-        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
-        
-        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
-        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
-        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
-        assert_create_file_with_empty_filename_response(response_data)
-        
-        # Дополнительная проверка структуры JSON, чтобы убедиться, что схема валидационного ответа не изменилась
-        validate_json_schema(response.json(), response_data.model_json_schema())
-
-    def test_create_file_with_empty_directory(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(
-            directory="",
-            upload_file="./testdata/files/image.png"
-        )
-        response = files_client.create_file_api(request)
-        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
-        
-        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
-        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
-        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
-        assert_create_file_with_empty_directory_response(response_data)
-        
-        # Дополнительная проверка структуры JSON
-        validate_json_schema(response.json(), response_data.model_json_schema())
-
+    @allure.tag(AllureTag.DELETE_ENTITY)
+    @allure.story(AllureStory.DELETE_ENTITY)
+    @allure.title("Delete file")
+    @allure.severity(Severity.NORMAL)
     def test_delete_file(self, files_client: FilesClient, function_file: FileFixture):
         # 1. Удаляем файл
         delete_response = files_client.delete_file_api(function_file.response.file.id)
@@ -83,6 +72,50 @@ class TestFiles:
         # 6. Проверяем, что ответ соответствует схеме
         validate_json_schema(get_response.json(), get_response_data.model_json_schema())
 
+    @allure.tag(AllureTag.VALIDATE_ENTITY)
+    @allure.story(AllureStory.VALIDATE_ENTITY)
+    @allure.title("Create file with empty filename")
+    @allure.severity(Severity.NORMAL)
+    def test_create_file_with_empty_filename(self, files_client: FilesClient):
+        request = CreateFileRequestSchema(
+            filename="",
+            upload_file="./testdata/files/image.png"
+        )
+        response = files_client.create_file_api(request)
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+        
+        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
+        assert_create_file_with_empty_filename_response(response_data)
+        
+        # Дополнительная проверка структуры JSON, чтобы убедиться, что схема валидационного ответа не изменилась
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    @allure.tag(AllureTag.VALIDATE_ENTITY)
+    @allure.story(AllureStory.VALIDATE_ENTITY)
+    @allure.title("Create file with empty directory")
+    @allure.severity(Severity.NORMAL)
+    def test_create_file_with_empty_directory(self, files_client: FilesClient):
+        request = CreateFileRequestSchema(
+            directory="",
+            upload_file="./testdata/files/image.png"
+        )
+        response = files_client.create_file_api(request)
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+        
+        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
+        assert_create_file_with_empty_directory_response(response_data)
+        
+        # Дополнительная проверка структуры JSON
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    @allure.tag(AllureTag.VALIDATE_ENTITY)
+    @allure.story(AllureStory.VALIDATE_ENTITY)
+    @allure.title("Get file with incorrect file id")
+    @allure.severity(Severity.NORMAL)
     def test_get_file_with_incorrect_file_id(self, files_client: FilesClient):
         response = files_client.get_file_api(file_id="incorrect-file-id")
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
