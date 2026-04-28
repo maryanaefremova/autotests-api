4c5f8462a5f6712f9d57e66c0fb1d4a6452654c2
maryanaefremova <maryana.efremova@gmail.com>
2026-04-28 13:30:30 +0300

    Настройки автотестов с Pydantic

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -2,6 +2,7 @@ import allure
 
 from clients.errors_schema import InternalErrorResponseSchema, ValidationErrorResponseSchema, ValidationErrorSchema
 from clients.files.files_schema import CreateFileResponseSchema, CreateFileRequestSchema, FileSchema, GetFileResponseSchema
+from config import settings
 from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_internal_error_response, assert_validation_error_response
 
@@ -15,8 +16,8 @@ def assert_create_file_response(request: CreateFileRequestSchema, response: Crea
     :param response: Ответ API с данными файла.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    # Формируем ожидаемую ссылку на загруженный файл
-    expected_url = f"http://localhost:8000/static/{request.directory}/{request.filename}"
+    # Используем значение хоста из настроек
+    expected_url = f"{settings.http_client.client_url}static/{request.directory}/{request.filename}"
 
     assert_equal(str(response.file.url), expected_url, "url")
     assert_equal(response.file.filename, request.filename, "filename")
